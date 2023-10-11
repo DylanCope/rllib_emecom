de2fc95f5f9ed643ef57459d4c71a645c31eb1e0
Dylan Cope <dylanr.cope@gmail.com>
2023-10-10 19:23:00 -0700

    Changed default channel conf

--- rllib_emecom/train/configs.py
+++ rllib_emecom/train/configs.py
@@ -37,9 +37,9 @@ def add_ppo_args(parser: ArgumentParser):
 
 
 def add_macrl_args(parser: ArgumentParser):
-    parser.add_argument('--message_dim', type=int, default=8)
+    parser.add_argument('--message_dim', type=int, default=32)
     parser.add_argument('--comm_channel_fn', type=str, default='gumbel_softmax')
-    parser.add_argument('--comm_channel_temp', type=float, default=10.0)
+    parser.add_argument('--comm_channel_temp', type=float, default=1.0)
     parser.add_argument('--comm_channel_noise', type=float, default=0.5)
     parser.add_argument('--comm_channel_activation', type=str, default='sigmoid')
     parser.add_argument('--no_param_sharing', action='store_true', default=False)
