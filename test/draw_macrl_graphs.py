31e56bcf7a5dc3495c35300670165548d5969a42
DylanCope <dylanr.cope@gmail.com>
2023-10-13 13:13:24 +0100

    Test code for illustrating macrl  computational graphs with different channel fns

---
+++ test/draw_macrl_graphs.py
@@ -0,0 +1,30 @@
+from .test_macrl_module import create_mock_macrl_module
+
+from pathlib import Path
+
+import torch
+from torchview import draw_graph
+from ray.rllib.core.models.base import SampleBatch
+
+
+def draw_macrl_graph(output_name: str, **kwargs):
+    macrl_module, _ = create_mock_macrl_module(**kwargs)
+    obs_space = macrl_module.config.observation_space
+    batch_size = 4
+    batch = {
+        SampleBatch.OBS: torch.vstack([
+            torch.tensor(obs_space.sample())
+            for _ in range(batch_size)
+        ]),
+    }
+
+    graph = draw_graph(macrl_module, {'batch': batch}, device='cpu', expand_nested=True)
+    graph.visual_graph.render(output_name, format='pdf', cleanup=True)
+
+
+if __name__ == '__main__':
+    outputs_dir = 'test/macrl_graphs'
+    Path(outputs_dir).mkdir(exist_ok=True)
+    for channel_fn in ['straight_through', 'dru', 'gumbel_softmax']:
+        draw_macrl_graph(f'{outputs_dir}/macrl_module_{channel_fn}',
+                         n_agents=2, channel_fn=channel_fn)
