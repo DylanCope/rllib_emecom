527eee1f573752446be00fb29a136de0fb56bfcb
Dylan Cope <dylanr.cope@gmail.com>
2023-10-26 07:47:45 -0700

    small changes to plotting graphs with different options

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,2 @@
-docker run --gpus all -it --rm --name rllib_emecom -p :8888:8888 -p :8265:8265 -v $(pwd)/:/home/ray/project --entrypoint=/bin/bash drc/rllib_emecom:latest
+# docker run --gpus all -it --rm --name rllib_emecom -p :8888:8888 -p :8265:8265 -v $(pwd)/:/home/ray/project --entrypoint=/bin/bash drc/rllib_emecom:latest
+docker run --privileged --gpus all -it --rm --name rllib_emecom -v $(pwd)/:/home/ray/project --entrypoint=/bin/bash drc/rllib_emecom:latest
--- test/draw_macrl_graphs.py
+++ test/draw_macrl_graphs.py
@@ -1,5 +1,6 @@
 from .test_macrl_module import create_mock_macrl_module
 
+import argparse
 from pathlib import Path
 
 import torch
@@ -7,7 +8,7 @@ from torchview import draw_graph
 from ray.rllib.core.models.base import SampleBatch
 
 
-def draw_macrl_graph(output_name: str, **kwargs):
+def draw_macrl_graph(output_name: str, graph_depth: int = 3, **kwargs):
     macrl_module, _ = create_mock_macrl_module(**kwargs)
     obs_space = macrl_module.config.observation_space
     batch_size = 4
@@ -18,12 +19,18 @@ def draw_macrl_graph(output_name: str, **kwargs):
         ]),
     }
 
-    graph = draw_graph(macrl_module, {'batch': batch}, device='cpu', expand_nested=True)
+    graph = draw_graph(macrl_module, {'batch': batch},
+                       depth=graph_depth, device='cpu', expand_nested=True)
     graph.visual_graph.render(output_name, format='pdf', cleanup=True)
 
 
 if __name__ == '__main__':
-    outputs_dir = 'test/macrl_graphs'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', type=str, default=None)
+    parser.add_argument('--graph_depth', type=int, default=3)
+    cmd_args = parser.parse_args()
+
+    outputs_dir = cmd_args.output_dir or 'test/macrl_graphs'
     Path(outputs_dir).mkdir(exist_ok=True)
 
     args = {
@@ -42,4 +49,5 @@ if __name__ == '__main__':
     for channel_fn in ['straight_through', 'dru', 'gumbel_softmax']:
         for i, channel_args in enumerate(args[channel_fn]):
             draw_macrl_graph(f'{outputs_dir}/macrl_module_{channel_fn}_{i}',
+                             graph_depth=cmd_args.graph_depth,
                              n_agents=2, channel_fn=channel_fn, **channel_args)
