60f1fa00d2997422ec99befcc2f8584f6c097292
DylanCope <dylanr.cope@gmail.com>
2023-10-28 16:33:39 +0100

    fixed formatting error

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         'ray[rllib]>=2.7.0rc0',
         'torch>=2.1.0',
         'gymnasium>=0.28.1',
-        'pettingzoo>=1.24.1'
+        'pettingzoo>=1.24.1',
         'numpy>=1.23.5',
         'pandas>=1.3.5',
         'networkx>=2.8.8',
