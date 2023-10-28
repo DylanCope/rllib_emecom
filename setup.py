d6a6432e6f109ea334e89af571a367852e5f37cc
Dylan Cope <dylanr.cope@gmail.com>
2023-10-28 15:43:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+  name="rllib_emecom",
+  version="0.0.1",
+  author="Dylan R. Cope"
+  description="Framwork for Emergent Communication using RLlib",
+  packages=["rllib_emecom"]
+)
